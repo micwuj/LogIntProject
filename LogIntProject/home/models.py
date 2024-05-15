f4896c90a8465fa962c1cd3a873aa5e2050ba8ec
micwuj <micwuj1@st.amu.edu.pl>
2024-05-15 19:29:23 +0200

    Integration model&tests added

--- LogIntProject/home/models.py
+++ LogIntProject/home/models.py
@@ -1,3 +1,15 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+class Integration(models.Model):
+    integration_name = models.CharField(max_length=200)
+    customer = models.CharField(max_length=200)
+    driver_id = models.IntegerField()
+    source = models.CharField(max_length=500)
+    apk_file = models.CharField(max_length=5000)
+    sh_script = models.CharField(max_length=5000)
+    is_active = models.BooleanField(default=True)
+    integration_date = models.DateField(default=datetime.now)
+
+    def __str__(self):
+        return self.integration_name
