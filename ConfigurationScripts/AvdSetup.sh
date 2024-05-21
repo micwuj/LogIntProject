808d751bd092d27c1d3bcaeb1cb8e119207c28b6
milosz-amg <milosz.rolewski01@gmail.com>
2024-05-21 10:53:51 +0200

    avd changed from 3a to 8 Pro

--- ConfigurationScripts/AvdSetup.sh
+++ ConfigurationScripts/AvdSetup.sh
@@ -2,7 +2,7 @@
 
 # Create a configuration file for the AVD
 CONFIG_FILE="device_config.ini"
-AVD_LOCATION="$HOME/.android/avd/Pixel_3a_API_34.avd"
+AVD_LOCATION="$HOME/.android/avd/Pixel_8_Pro_API_34.avd"
 
 # Create the AVD
 # avdmanager --verbose create avd --force --name "executoner" --package "system-images;android-34;google_apis_playstore;x86_64" --tag "google_apis_playstore"
